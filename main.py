222ea15beed44931e9cc4048eeb2e146e8edae46
ygarrot <garrot.yoan1@gmail.com>
2021-10-14 13:58:01 +0200

    cleaner functions

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 # import seaborn; seaborn.set()
 
-# ['EAg', 'EAn', 'Egx', 'Chgx', 'ChAg', 'ChAn', 'EtAG', 'EtGx', 'P0An']
-# ['Rumination', 'Ingestion', 'OverActivity', 'Other Activity', 'Rest']
+# raw = ['EAg', 'EAn', 'Egx', 'Chgx', 'ChAg', 'ChAn', 'EtAG', 'EtGx', 'P0An']
+# normalized = ['Rumination', 'Ingestion', 'OverActivity', 'Other Activity', 'Rest']
 root = 'hectare_data/heatlive_allaitant_ms/'
 metadata_csv = root + 'metadata.csv'
 normalized = root + 'normalized/'
@@ -39,10 +39,9 @@ def state_pair_plot(normalized_events, raw_events):
     # pd.plotting.autocorrelation_plot(normalized_events['Rumination'])
     state_dic = state_dic.apply(set_state, axis=1)
     state_dic = state_dic.drop(columns=['Rumination', 'Ingestion_at_trough',
-                                        'Ingestion_at_pasture', 'OverActivity', 'Rest', 'Up', 'OtherActivity'])
+                                        'Ingestion_at_pasture', 'OverActivity',
+                                        'Rest', 'Up', 'OtherActivity'])
     state_dic = state_dic.interpolate().dropna()
-
-
     sns.pairplot(state_dic, hue='State')
     plt.show()
 
@@ -50,17 +49,22 @@ def state_pair_plot(normalized_events, raw_events):
 def plot_all(normalized_events, raw_events):
         print(normalized_events.shape)
         normalized_events_len = normalized_events.shape[1]
-        fig, axes = plt.subplots(normalized_events_len + raw_events.shape[1], 1, sharex=True)
-        normalized_events.plot(ax=axes[:normalized_events_len], subplots=True)
-        raw_events.plot(ax=axes[normalized_events_len:], subplots=True)
+        # fig, axes = plt.subplots(normalized_events_len + raw_events.shape[1], 1, sharex=True)
+        # normalized_events.plot(ax=axes[:normalized_events_len], subplots=True)
+        # raw_events.plot(ax=axes[normalized_events_len:], subplots=True)
 
         # raw_events.hist()
         # normalized_events.hist()
         # sns.pairplot(raw_events, hue='State')
-        # plt.plot_date(normalized_events, normalized_events.index)
+        print(normalized_events['Rumination'])
+
+        plt.plot_date(normalized_events.index,
+                      normalized_events['Rumination'].astype('int'), 
+                      linestyle='--')
 
         ## LAG PLOT ##
         # pd.plotting.lag_plot(raw_events['ChAg'], lag=1)
+
         plt.show()
 
 def floor_events(*events):
@@ -68,26 +72,29 @@ def floor_events(*events):
             event.index = event.index.floor('5T').floor('Min') 
         return events
 
-
 #     dic = {}
 #     for index, row in csv.sort_index().iterrows():
-def plot_one(csv):
-    row = csv.iloc[0].sort_index()
+def plot_one(row):
     event = row['event_id'] 
     normalized_events = pd.read_parquet(normalized + event + '.parquet').fillna(False)
     # raw_events = pd.read_parquet(ax + event + '.parquet')[['EAn', 'EAg', 'Egx']].interpolate()
     raw_events = pd.read_parquet(ax + event + '.parquet').interpolate()
+    if (normalized_events.shape[0] < 1):
+        return
+    print(normalized_events.index[0])
     normalized_events = normalized_events.loc['2019-02-06':'2019-02-07']
     raw_events = raw_events.loc['2019-02-06':'2019-02-07']
     # raw_events = rename_raw_data(raw_events)
     raw_events, normalized_events = floor_events(raw_events, normalized_events)
 
-    plot_all(normalized_events, raw_events)
-    state_pair_plot(normalized_events, raw_events)
+    # plot_all(normalized_events, raw_events)
+    # state_pair_plot(normalized_events, raw_events)
 
 def main():
     csv = pd.read_csv(metadata_csv)
-    plot_one(csv)
+    # plot_one(csv.iloc[0].sort_index())
+    for index, row in csv.sort_index().iterrows():
+        plot_one(row)
 
 if __name__ == '__main__':
     main()
